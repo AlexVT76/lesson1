38668158b86a4193315ab9a8e68e5ded3207a52b
AlexVT76 <tokarevalex76@gmail.com>
2024-09-15 16:21:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+example = "Индексация"# This is a sample Python script.
+print (example[0])
+print (example[-1])
+print (example[len(example)//2:])
+print (example[::-1])
+print (example[1::2])
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
